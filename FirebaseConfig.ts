ba98c2752b92216c9bd9f127f48a6da1a9e96501
Murilo Anjos <com>
2023-09-19 23:28:31 -0300

    chore: :wrench: add fire base config

---
+++ FirebaseConfig.ts
@@ -0,0 +1,16 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBGAyQy71idMqKCK5xb-NrN2zxVrBJ1h7k",
+  authDomain: "compasssales-98bca.firebaseapp.com",
+  projectId: "compasssales-98bca",
+  storageBucket: "compasssales-98bca.appspot.com",
+  messagingSenderId: "903007793738",
+  appId: "1:903007793738:web:7321193ef29f854f2cacb3"
+};
+
+// Initialize Firebase
+export const FIREBASE_APP = initializeApp(firebaseConfig);
